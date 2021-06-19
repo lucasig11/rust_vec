da6607e9c4aeabdf399dd0f75c5f6b7f5bbd95e7
lucas <lucasfonsecard@icloud.com>
2021-06-18 22:21:51 -0300

    add basic vec methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,161 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(ptr_internals)]
+#![feature(allocator_api)]
+
+use std::{
+    alloc::{handle_alloc_error, Allocator, Global, Layout},
+    mem,
+    ops::{Deref, DerefMut},
+    ptr::{self, NonNull, Unique},
+};
+
+pub struct Vec<T> {
+    // pointer to the allocation
+    ptr: Unique<T>,
+
+    // size of allocation
+    cap: usize,
+
+    // number of initializes elements
+    len: usize,
 }
+
+impl<T> Vec<T> {
+    pub fn new() -> Self {
+        assert!(mem::size_of::<T>() != 0, "zero-sized type not allowed..yet");
+        // Create a dangling pointer in the place of the pointer
+        // because we should allocate nothing on ::new
+        Vec {
+            ptr: Unique::dangling(),
+            len: 0,
+            cap: 0,
+        }
+    }
+
+    fn grow(&mut self) {
+        unsafe {
+            let elem_size = mem::size_of::<T>();
+
+            let (new_cap, ptr) = if self.cap == 0 {
+                let ptr = Global.allocate(Layout::array::<T>(1).unwrap());
+                (1, ptr)
+            } else {
+                let new_cap = 2 * self.cap;
+                let old_num_bytes = self.cap * elem_size;
+
+                assert!(
+                    old_num_bytes <= (isize::MAX as usize) / 2,
+                    "capacity overflow"
+                );
+
+                let c: NonNull<T> = self.ptr.into();
+                let ptr = Global.grow(
+                    c.cast(),
+                    Layout::array::<T>(self.cap).unwrap(),
+                    Layout::array::<T>(new_cap).unwrap(),
+                );
+
+                (new_cap, ptr)
+            };
+
+            // Out of memory
+            if ptr.is_err() {
+                handle_alloc_error(Layout::from_size_align_unchecked(
+                    new_cap * elem_size,
+                    mem::align_of::<T>(),
+                ))
+            }
+
+            let ptr = ptr.unwrap();
+
+            self.ptr = Unique::new_unchecked(ptr.as_ptr() as *mut _);
+            self.cap = new_cap;
+        }
+    }
+}
+
+impl<T> Vec<T> {
+    pub fn push(&mut self, elem: T) {
+        if self.len == self.cap {
+            self.grow()
+        };
+
+        unsafe {
+            ptr::write(self.ptr.as_ptr().offset(self.len as isize), elem);
+        }
+
+        self.len += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        if self.len == 0 {
+            None
+        } else {
+            self.len -= 1;
+            unsafe { Some(ptr::read(self.ptr.as_ptr().offset(self.len as isize))) }
+        }
+    }
+
+    pub fn insert(&mut self, index: usize, elem: T) {
+        assert!(index <= self.len, "Index out of bounds");
+
+        if self.cap == self.len {
+            self.grow();
+        }
+
+        unsafe {
+            if index < self.len {
+                // ptr::copy(source, dest, count) > Copy from source to dest count elements
+                ptr::copy(
+                    self.ptr.as_ptr().offset(index as isize),
+                    self.ptr.as_ptr().offset(index as isize + 1),
+                    self.len - index,
+                );
+            }
+
+            ptr::write(self.ptr.as_ptr().offset(index as isize), elem);
+            self.len += 1;
+        }
+    }
+
+    pub fn remove(&mut self, index: usize) -> T {
+        assert!(index < self.len, "index out of bounds");
+
+        unsafe {
+            self.len -= 1;
+            ptr::copy(
+                self.ptr.as_ptr().offset(index as isize + 1),
+                self.ptr.as_ptr().offset(index as isize),
+                self.len - index,
+            );
+            ptr::read(self.ptr.as_ptr().offset(index as isize))
+        }
+    }
+}
+
+impl<T> Drop for Vec<T> {
+    fn drop(&mut self) {
+        if self.cap != 0 {
+            while let Some(_) = self.pop() {}
+
+            unsafe {
+                let c: NonNull<T> = self.ptr.into();
+                Global.deallocate(c.cast(), Layout::array::<T>(self.cap).unwrap());
+            }
+        }
+    }
+}
+
+impl<T> Deref for Vec<T> {
+    type Target = [T];
+    fn deref(&self) -> &[T] {
+        unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.len) }
+    }
+}
+
+impl<T> DerefMut for Vec<T> {
+    fn deref_mut(&mut self) -> &mut [T] {
+        unsafe { std::slice::from_raw_parts_mut(self.ptr.as_ptr(), self.len) }
+    }
+}
+
+fn main() {}
